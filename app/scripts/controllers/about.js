73b685ef6f4511d4ef8643d381c5c6c8bc335988
Pedro Lopes <pedrompflopes@gmail.com>
2015-08-18 14:01:28 +0100

    base angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carminaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the carminaApp
+ */
+angular.module('carminaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carminaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the carminaApp
+ */
+angular.module('carminaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
